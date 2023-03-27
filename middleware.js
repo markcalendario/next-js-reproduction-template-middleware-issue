698695fa7cec400504afab2637b97842044116ac
markcalendario <markcalendario@gmail.com>
2023-03-27 22:11:52 +0800

    make initial commit

---
+++ middleware.js
@@ -0,0 +1,13 @@
+// middleware.ts
+
+// import { NextResponse } from 'next/server'
+
+export function middleware(request) {
+	// if (request.nextUrl.pathname.startsWith('/feed')) {
+	// 	return NextResponse.redirect(new URL('/', request.url))
+	// }
+
+	// if (request.nextUrl.pathname.startsWith('/dashboard')) {
+	// 	return NextResponse.rewrite(new URL('/dashboard/user', request.url))
+	// }
+}
